9bf66c18cc3811556cbd439dd7d51e8169ae9727
senkop <77180411+senkop@users.noreply.github.com>
2022-02-27 10:49:23 +0200

    first commit

---
+++ BL/Mapper/DomainProfile.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+//using Try.DAL.Entities;
+using Try.DAL.Entity;
+using Try.Models;
+
+namespace Try.BL.Mapper
+{
+    public class DomainProfile : Profile
+    {
+     
+        
+        public DomainProfile()
+        {
+            CreateMap<Ads, AdsVM>();
+            CreateMap<AdsVM, Ads>();
+
+            CreateMap<Client_interesets, Client_interesetsVM>();
+            CreateMap<Client_interesetsVM, Client_interesets>();
+
+            CreateMap<Ads, AdsVM>();
+            CreateMap<AdsVM, Ads>();
+
+            CreateMap<Client_orders, Client_ordersVM>();
+            CreateMap<Client_ordersVM, Client_orders>();
+
+            CreateMap<Ads, AdsVM>();
+            CreateMap<AdsVM, Ads>();
+
+            CreateMap<Clients, ClientsVM>();
+            CreateMap<ClientsVM, Clients>();
+
+            CreateMap<Estate, EstateVM>();
+            CreateMap<EstateVM, Estate>();
+
+            CreateMap<Feedback, FeedbackVM>();
+            CreateMap<FeedbackVM, Feedback>();
+
+            CreateMap<Interests, InterestsVM>();
+            CreateMap<InterestsVM, Interests>();
+
+            CreateMap<Order_Estate, Order_EstateVM>();
+            CreateMap<Orders, Order_Estate>();
+
+
+            CreateMap<Orders, OrdersVM>();
+            CreateMap<OrdersVM, Orders>();
+
+            CreateMap<Properties, PropertiesVM>();
+            CreateMap<PropertiesVM, Properties>();
+
+            CreateMap<UserGroup, UserGroupVM>();
+            CreateMap<UserGroupVM, UserGroup>();
+
+
+            CreateMap<Users, UsersVM>();
+            CreateMap<UsersVM, Users>();
+
+        }
+
+    }
+}
